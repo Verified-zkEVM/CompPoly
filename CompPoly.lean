d60b3359df849bbe01ff52c20b8a0c466855c18b
Frantisek Silvasi <silvasi.frantisek@gmail.com>
2025-09-08 09:49:52 +0200

    prune redundant files

--- CompPoly.lean
+++ CompPoly.lean
@@ -1,8 +1,8 @@
 -- This module serves as the root of the `CompPoly` library.
 -- Import modules here that should be built as part of the library.
 import CompPoly.CMvMonomial
-import CompPoly.Unlawful
-import CompPoly.Lawful
 import CompPoly.CMvPolynomial
+import CompPoly.Lawful
 import CompPoly.MvPolyEquiv
+import CompPoly.Unlawful
 import CompPoly.Wheels
