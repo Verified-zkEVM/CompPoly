406ae9c685ef7a0c1f8393232921315926c5ae98
Chung Thai Nguyen <chung.thai.ng@gmail.com>
2025-12-09 13:39:05 +0000

    chore: bump lean to v4.22.0

--- lakefile.lean
+++ lakefile.lean
@@ -4,7 +4,8 @@ open System Lake DSL
 
 package CompPoly where version := v!"0.1.0"
 
-require "leanprover-community" / mathlib @ git "v4.22.0-rc2"
+require "leanprover-community" / mathlib @ git "v4.22.0"
+require "leanprover-community" / batteries @ git "v4.22.0"
 
 require ExtTreeMapLemmas from git "https://github.com/NethermindEth/ExtTreeMapLemmas" @ "Ferinko/cleanup"
 
