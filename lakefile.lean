690923780b74a09d6a83c68dcec4dd5b9fc2a17e
Derek Sorensen <d@dhsorens.com>
2026-01-19 13:17:37 +0000

    fix: moved fork into verified-zkevm and ready to upgrade now

--- lakefile.lean
+++ lakefile.lean
@@ -6,7 +6,7 @@ package CompPoly where version := v!"0.1.0"
 
 require "leanprover-community" / mathlib @ git "v4.26.0"
 
-require ExtTreeMapLemmas from git "https://github.com/dhsorens/ExtTreeMapLemmas"
+require ExtTreeMapLemmas from git "https://github.com/Verified-zkEVM/ExtTreeMapLemmas"
 
 @[default_target]
 lean_lib CompPoly
