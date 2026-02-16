9df392b60c2ac9b4b130e23051b5d8fcc686427c
Derek Sorensen <derek.sorensen@ethereum.org>
2026-02-16 13:09:55 +0000

    update authors (#98)

--- CompPoly/Multivariate/Restrict.lean
+++ CompPoly/Multivariate/Restrict.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2025 CompPoly. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Natalia Klaus, Frantisek Silvasi, Derek Sorensen
+Authors: Fawad Haider
 -/
 import CompPoly.Multivariate.CMvPolynomial
 
