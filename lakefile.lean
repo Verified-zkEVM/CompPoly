c0e45b11b071dfd060f5e2422b2307cbc0910f50
Derek Sorensen <d@dhsorens.com>
2026-02-19 10:44:47 +0000

    feat: bump versions to 4_27

--- lakefile.lean
+++ lakefile.lean
@@ -4,9 +4,9 @@ open System Lake DSL
 
 package CompPoly where version := v!"0.1.0"
 
-require "leanprover-community" / mathlib @ git "v4.26.0"
+require "leanprover-community" / mathlib @ git "v4.27.0"
 
-require ExtTreeMapLemmas from git "https://github.com/Verified-zkEVM/ExtTreeMapLemmas"
+require ExtTreeMapLemmas from git "https://github.com/Verified-zkEVM/ExtTreeMapLemmas"@"v4.27.0"
 
 @[default_target]
 lean_lib CompPoly
