26efaac64181e579af7b9e8bd97cf03ba63b9bfc
Julian Sutherland <julian@nethermind.io>
2026-02-23 15:06:36 +0000

    more cleanup

--- CompPoly/Univariate/Lagrange.lean
+++ CompPoly/Univariate/Lagrange.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2025 CompPoly. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Quang Dao, Gregor Mitscha-Baude, Derek Sorensen, Katerina Hristova, Julian Sutherland.
+Authors: Quang Dao, Gregor Mitscha-Baude, Derek Sorensen, Katerina Hristova, Julian Sutherland
 -/
 import Mathlib.LinearAlgebra.Lagrange
 import CompPoly.Univariate.Basic
