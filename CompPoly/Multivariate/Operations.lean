e222544a34bf1131ea8dbb49743a615ed805a5ef
Dimitris <di.mitsios@gmail.com>
2026-02-28 11:58:19 +0200

    chore: added authorship to Operations

--- CompPoly/Multivariate/Operations.lean
+++ CompPoly/Multivariate/Operations.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2025 CompPoly. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Frantisek Silvasi, Julian Sutherland, Andrei Burdușa, Derek Sorensen
+Authors: Frantisek Silvasi, Julian Sutherland, Andrei Burdușa, Derek Sorensen, Dimitris Mitsios
 -/
 import CompPoly.Multivariate.MvPolyEquiv
 
