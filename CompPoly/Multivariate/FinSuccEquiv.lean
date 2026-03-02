884e3aeddbb0672561cc24bd2591c3cf41a12ec7
Elias Judin <ejudin@gmail.com>
2026-03-02 14:04:07 +0200

    Update authors in FinSuccEquiv.lean

--- CompPoly/Multivariate/FinSuccEquiv.lean
+++ CompPoly/Multivariate/FinSuccEquiv.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2025 CompPoly. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: CompPoly Contributors
+Authors: Elias Judin, Aristotle (Harmonic)
 -/
 import CompPoly.Multivariate.MvPolyEquiv
 import Mathlib.Algebra.MvPolynomial.Equiv
