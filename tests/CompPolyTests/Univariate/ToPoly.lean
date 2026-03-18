a6078d7c4f076b1b99c6487a54d83f12634bc465
Derek Sorensen <d@dhsorens.com>
2026-03-18 15:07:56 +0000

    fix: failing test

--- tests/CompPolyTests/Univariate/ToPoly.lean
+++ tests/CompPolyTests/Univariate/ToPoly.lean
@@ -3,7 +3,7 @@ Copyright (c) 2026 CompPoly. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Derek Sorensen
 -/
-import CompPoly.Univariate.ToPoly.Equiv
+import CompPoly.Univariate.ToPoly.Impl
 
 /-!
   # Univariate ToPoly Tests
