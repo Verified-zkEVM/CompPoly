8a106755425f6205cdaa94896baa936f40555e81
erdemkan <salih.kocak.0733@gmail.com>
2026-03-21 20:45:11 +0300

    add authors

--- tests/CompPolyTests/Univariate/NTT/Benchmark.lean
+++ tests/CompPolyTests/Univariate/NTT/Benchmark.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2026 CompPoly. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: CompPoly Contributors
+Authors: Salih Erdem Koçak, Doran Pamukçu
 -/
 
 import CompPoly.Univariate.NTT.FastMul
