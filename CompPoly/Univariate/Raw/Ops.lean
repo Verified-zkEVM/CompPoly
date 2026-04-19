898f3617b898467b96b096dce35833c3fbe26e11
Dimitris <di.mitsios@gmail.com>
2026-04-19 19:58:24 +0300

    added myself to the author list

--- CompPoly/Univariate/Raw/Ops.lean
+++ CompPoly/Univariate/Raw/Ops.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2025 CompPoly. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Quang Dao, Gregor Mitscha-Baude, Derek Sorensen, Desmond Coles, Natalie Klaus
+Authors: Quang Dao, Gregor Mitscha-Baude, Derek Sorensen, Desmond Coles, Natalie Klaus, Dimitris Mitsios
 -/
 import CompPoly.Univariate.Raw.Core
 
