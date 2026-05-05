bc5fb5f3b2ce0313edc3fd693e956dacf1443e25
Derek Sorensen <derek.sorensen@ethereum.org>
2026-05-05 10:53:09 +0100

    feat: update to 4.29.1 (#207)

--- lakefile.lean
+++ lakefile.lean
@@ -6,9 +6,9 @@ package CompPoly where
   version := v!"0.1.0"
   testDriver := "CompPolyTests"
 
-require "leanprover-community" / mathlib @ git "v4.29.0"
+require "leanprover-community" / mathlib @ git "v4.29.1"
 
-require ExtTreeMapLemmas from git "https://github.com/Verified-zkEVM/ExtTreeMapLemmas"@"v4.29.0"
+require ExtTreeMapLemmas from git "https://github.com/Verified-zkEVM/ExtTreeMapLemmas"@"v4.29.1"
 
 @[default_target]
 lean_lib CompPoly
