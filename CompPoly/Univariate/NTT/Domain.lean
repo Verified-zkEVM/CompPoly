0098bfa86fd82c743ae532512e26287df373dd04
Valerii Huhnin <olympichek1@gmail.com>
2026-05-15 12:34:48 +0000

    Add co-author headers

--- CompPoly/Univariate/NTT/Domain.lean
+++ CompPoly/Univariate/NTT/Domain.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2026 CompPoly. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Salih Erdem Koçak, Doran Pamukçu
+Authors: Salih Erdem Koçak, Doran Pamukçu, Valerii Huhnin
 -/
 import CompPoly.Univariate.Raw
 import Mathlib.Data.Nat.Log
