f9304fbc0acf88d86aaadab880a14c98f6cbb6ce
Dimitris <di.mitsios@gmail.com>
2026-05-15 19:57:53 +0300

    feat: add, mul, zero added, constant unfinished

--- CompPoly/Bivariate/CMvEquiv.lean
+++ CompPoly/Bivariate/CMvEquiv.lean
@@ -5,13 +5,37 @@ Authors: Dimitris Mitsios
 -/
 
 import CompPoly.Bivariate.Basic
+import CompPoly.Bivariate.ToPoly
 import CompPoly.Multivariate.CMvPolynomial
+import CompPoly.Multivariate.MvPolyEquiv.Instances
+import CompPoly.Multivariate.FinSuccEquiv
 
-open CompPoly CPoly
+open CompPoly CPoly CBivariate
 
-variable {R : Type*} [CommSemiring R]
-
-noncomputable def univariateEquiv
+variable {R : Type*}
+variable [CommRing R] [BEq R] [LawfulBEq R] [Nontrivial R] [DecidableEq R]
 
 /- theorem CBivariate_eq_CMvPolynomial {R : Type*}  [CommSemiring R] [BEq R] [LawfulBEq R] : CBivariate R ≃+* CMvPolynomial 2 R := by sorry -/
-noncomputable def bivariateEquiv {R : Type*} [Semiring R] : CBivariate R ≃+* CMvPolynomial 2 R := sorry
+noncomputable def bivariateEquiv : CBivariate R ≃+* CMvPolynomial 2 R :=
+  let step1 := CBivariate.ringEquiv
+  let step2 := Polynomial.mapEquiv (Polynomial.mapEquiv CMvPolynomial.isEmptyRingEquiv.symm)
+  let step3 := Polynomial.mapEquiv (CMvPolynomial.finSuccEquiv (n:=0) (R:=R)).symm
+  let step4 := (CMvPolynomial.finSuccEquiv (n:=1) (R:=R)).symm
+  step1.trans <| step2.trans <| step3.trans step4
+
+lemma bivariateEquiv_add (p q : CBivariate R) :
+  bivariateEquiv (p + q) = bivariateEquiv p + bivariateEquiv q :=
+    bivariateEquiv.map_add p q
+
+lemma bivariateEquiv_mul (p q : CBivariate R) :
+  bivariateEquiv (p * q) = bivariateEquiv p *  bivariateEquiv q :=
+    bivariateEquiv.map_mul p q
+
+lemma bivariateEquiv_zero : bivariateEquiv (0 : CBivariate R) = 0 :=
+  map_zero bivariateEquiv
+
+lemma bivariateEquiv_CC (r : R) : bivariateEquiv (CBivariate.CC r) = CMvPolynomial.C r := by
+  unfold CC
+
+
+#check toPoly
