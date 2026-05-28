9723338a284e22ebeea8e64b4975e9829f5fa2a8
Juan C <juanconrod@protonmail.com>
2026-05-28 18:29:53 +0200

    `DivisionCorrectness.lean`: Authorship

--- CompPoly/Univariate/DivisionCorrectness.lean
+++ CompPoly/Univariate/DivisionCorrectness.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2026 CompPoly. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Valerii Huhnin
+Authors: Valerii Huhnin, Juan Conejero
 -/
 import Mathlib.Algebra.Polynomial.Div
 import Mathlib.Algebra.Polynomial.FieldDivision
