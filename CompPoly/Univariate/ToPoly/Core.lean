4599fe3ac87985659f80125afb41d96c10ba5b20
Dimitris <di.mitsios@gmail.com>
2026-06-03 20:07:27 +0300

    fix: remove dead import

--- CompPoly/Univariate/ToPoly/Core.lean
+++ CompPoly/Univariate/ToPoly/Core.lean
@@ -3,7 +3,6 @@ Copyright (c) 2025 CompPoly. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Quang Dao, Gregor Mitscha-Baude, Derek Sorensen
 -/
-import Mathlib.Algebra.Polynomial.Derivative
 import Mathlib.Algebra.Polynomial.Inductions
 import Mathlib.Algebra.Ring.TransferInstance
 import Mathlib.Algebra.Tropical.Basic
