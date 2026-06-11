23c8741a22744569b917b81658d144210ed87f34
Valerii Huhnin <olympichek1@gmail.com>
2026-06-11 14:40:05 +0000

    List all modules in facade files

--- CompPoly/Fields/Binary/GF2_32.lean
+++ CompPoly/Fields/Binary/GF2_32.lean
@@ -4,6 +4,13 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Valerii Huhnin
 -/
 
+import CompPoly.Fields.Binary.GF2_32.Prelude
+import CompPoly.Fields.Binary.GF2_32.Basic
+import CompPoly.Fields.Binary.GF2_32.Impl
+import CompPoly.Fields.Binary.GF2_32.XPowTwoPowModCertificate
+import CompPoly.Fields.Binary.GF2_32.XPowTwoPowGcdCertificate
+import CompPoly.Fields.Binary.GF2_32.PrimitivePowerCertificate
+import CompPoly.Fields.Binary.GF2_32.Primitive
 import CompPoly.Fields.Binary.GF2_32.RootContexts
 
 /-! # GF(2^32) Binary Field Facade -/
